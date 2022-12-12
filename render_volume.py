720b750ad905a195018c99c37996805acf8c7f99
Lee Sharma <lee@leesharma.com>
2022-12-12 11:38:23 -0500

    Fix disp map rendering for single frame
    This undoes my normalization, so creating a disp maps should work. I'll
    try to add it back in correctly later (probably clipping would fix it?)

--- render_volume.py
+++ render_volume.py
@@ -388,7 +388,7 @@ def main(cfg: HydraConfig = None):
             logger.info(f'Done, saving {rgbs.shape} {disps.shape}')
             moviebase = os.path.join(basedir, expname, '{}_spiral_{:06d}_'.format(expname, i))
             imageio.mimwrite(moviebase + 'rgb.mp4', to8b(rgbs), fps=30, quality=8)
-            imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps / np.max(disps)), fps=30, quality=8)
+            imageio.mimwrite(moviebase + 'disp.mp4', to8b(disps), fps=30, quality=8)
 
             if cfg.nerf.rendering.use_viewdirs:
                 render_kwargs_test['c2w_staticcam'] = render_poses[0][:3,:4]
